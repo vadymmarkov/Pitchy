1a878a74dabf05897d89dadae0f8a28d439d066c
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 02:56:37 +0100

    Sound offsets

--- Source/Sound.swift
+++ Source/Sound.swift
@@ -2,25 +2,39 @@ import Foundation
 
 public struct Sound {
 
-  public var pitch: Pitch
-  public var adjoinedPitch: Pitch
-  public var frequency: Double
-  public var frequencyOffset: Double
-  public var percentageOffset: Double
+  public typealias Offset = (
+    pitch: Pitch,
+    frequency: Double,
+    percentage: Double
+  )
+
+  public struct Offsets {
+    let left: Sound.Offset
+    let right: Sound.Offset
+
+    public var closest: Sound.Offset {
+      return abs(left.frequency) < abs(right.frequency)
+        ? left : right
+    }
+
+    public init(left: Offset, right: Offset) {
+      if left.pitch.frequency < right.pitch.frequency {
+        self.left = left
+        self.right = right
+      } else {
+        self.left = right
+        self.right = left
+      }
+    }
+  }
+
+  public let frequency: Double
+  public let offsets: Offsets
 
   // MARK: - Initialization
 
-  public init(note: Note, octave: Int) {
-    self.note = note
-    self.octave = octave
-    index = PitchCalculator.index(note: note, octave: octave)
-    frequency = PitchCalculator.frequency(index: index)
-  }
-
   public init(frequency: Double) {
     self.frequency = frequency
-    index = PitchCalculator.index(frequency: frequency)
-    note = PitchCalculator.note(index: index)
-    octave = PitchCalculator.octave(index: index)
+    offsets = PitchCalculator.offsets(frequency)
   }
 }
