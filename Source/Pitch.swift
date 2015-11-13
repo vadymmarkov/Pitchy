d68af474947d4c5c420e43551d9638f432814c56
Vadym Markov <markov.vadym@gmail.com>
2015-11-13 02:56:03 +0100

    Modify Pitch

--- Source/Pitch.swift
+++ Source/Pitch.swift
@@ -2,24 +2,41 @@ import Foundation
 
 public struct Pitch {
 
-  public var index: Int
-  public var note: Note
-  public var octave: Int
-  public var frequency: Double
+  public let index: Int
+  public let note: Note
+  public let octave: Int
+  public let frequency: Double
+
+  public var previousPitch: Pitch {
+    let pitch = Pitch(index: index - 1)
+    return pitch
+  }
+
+  public var nextPitch: Pitch {
+    let pitch = Pitch(index: index + 1)
+    return pitch
+  }
 
   // MARK: - Initialization
+
+  public init(index: Int) {
+    self.index = index
+    self.note = PitchCalculator.note(index: index)
+    self.octave = PitchCalculator.octave(index: index)
+    self.frequency = PitchCalculator.frequency(index: index)
+  }
+
+  public init(frequency: Double) {
+    index = PitchCalculator.index(frequency: frequency)
+    note = PitchCalculator.note(index: index)
+    octave = PitchCalculator.octave(index: index)
+    self.frequency = PitchCalculator.frequency(index: index)
+  }
 
   public init(note: Note, octave: Int) {
     self.note = note
     self.octave = octave
     index = PitchCalculator.index(note: note, octave: octave)
     frequency = PitchCalculator.frequency(index: index)
-  }
-
-  public init(frequency: Double) {
-    self.frequency = frequency
-    index = PitchCalculator.index(frequency: frequency)
-    note = PitchCalculator.note(index: index)
-    octave = PitchCalculator.octave(index: index)
   }
 }
