a028a12ac9fdb1bbd560884b5dad7019d78ed3f4
Vadym Markov <markov.vadym@gmail.com>
2015-11-17 00:33:51 +0100

    Add base

---
+++ Source/AcousticWave.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct AcousticWave {
+
+  public let frequency: Double
+  public let wavelength: Double
+  public let period: Double
+
+  public init(frequency: Double) {
+
+  }
+
+  public init(wavelength: Double) {
+
+  }
+
+  public init(period: Double) {
+
+  }
+}
