338c4b0d4ecd199db73ba87443f9d52d4a9a4510
andy bezaire <andy.bezaire@gmail.com>
2022-02-06 12:47:47 +0200

    Distribute as a library via SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
-  name: "Pitchy"
+    name: "Pitchy",
+    products: [ .library(name: "Pitchy", targets: ["Pitchy"])],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.1.0"),
+    ],
+    targets: [
+        .target(name: "Pitchy", dependencies: [], path: "Source"),
+        .testTarget(name: "PitchyTests", dependencies: ["Pitchy", "Quick", "Nimble"], path: "PitchyTests", exclude: ["iOS/Info.plist", "Mac/Info.plist"])
+    ]
 )
