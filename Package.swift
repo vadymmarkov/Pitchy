140e69f8b70fb4563b56150f7dc15f4371c1a051
Antonio Candinho <antonio@goodnotesapp.com>
2023-02-05 08:11:58 +0100

    Update SwiftPM support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:5.5.0
+
 import PackageDescription
 
 let package = Package(
-  name: "Pitchy"
+  name: "Pitchy",
+  products: [
+    .library(name: "Pitchy", targets: ["Pitchy"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Quick/Quick", from: "6.1.0"),
+    .package(url: "https://github.com/Quick/Nimble", from: "11.2.1"),
+  ],
+  targets: [
+    .target(name: "Pitchy", dependencies: [], path: "Source"),
+    .testTarget(
+      name: "PitchyTests",
+      dependencies: ["Pitchy", "Quick", "Nimble"],
+      path: "PitchyTests",
+      exclude: [ "Mac/Info.plist", "iOS/Info.plist" ]
+    ),
+  ]
 )
